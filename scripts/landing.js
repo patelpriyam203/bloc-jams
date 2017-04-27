a88917afe433be0183e52fae5cdb9a43ab9325d6
PRIYAM PATEL <PATELPRIYAM203@GMAIL.COM>
2017-04-27 18:46:45 -0400

    Assignment 9 Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,38 +1,30 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-           var pointsArray = document.getElementsByClassName('point');
-
-           var animatePoints = function(points) {
-
-           var revealPoint = function(index) {
-             points[index].style.opacity = 1;
-             points[index].style.transform = "scaleX(1) translateY(0)";
-             points[index].style.msTransform = "scaleX(1) translateY(0)";
-             points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-           };
-
-           for(var i=0; i<points.length; i++){
-             revealPoint(i);
-           }
+var revealPoint = function(point) {
+  point.style.opacity = 1;
+  point.style.transform = "scaleX(1) translateY(0)";
+  point.style.msTransform = "scaleX(1) translateY(0)";
+  point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
 
 
-         };
+var animatePoints = function(points) {
+  forEach(points, revealPoint);
+};
 
-           window.onload = function() {
 
-             if (window.innerHeight > 950) {
-               animatePoints(pointsArray);
-             }
+window.onload = function() {
 
-             var sellingPoints = document.getElementsByClassName("selling-points")[0];
-             var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+  if (window.innerHeight > 950) {
+    animatePoints(pointsArray);
+  }
 
-             window.addEventListener('scroll', function(event){
-               if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-                 animatePoints(pointsArray);
-               }
-             });
-           };
+  var sellingPoints = document.getElementsByClassName("selling-points")[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
-}
-animatePoints();
+  window.addEventListener('scroll', function(event){
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+      animatePoints(pointsArray);
+    }
+  });
+};
